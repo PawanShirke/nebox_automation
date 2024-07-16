dcee32e14d29fee3de8c2731327685c7cff3a747
PawanShirke <45839240+PawanShirke@users.noreply.github.com>
2024-07-16 22:47:28 +0530

    Add files via upload (#1)

--- Automate-everything/fetch_data/vlans.py
+++ Automate-everything/fetch_data/vlans.py
@@ -1,6 +1,6 @@
 from fetch_data import get_netbox_session
 
-def get_vlans_by_site(site_id):
+def get_vlans_by_site():
     netbox_session = get_netbox_session()
-    vlans = list(netbox_session.ipam.vlans.filter(site_id=site_id))
+    vlans = list(netbox_session.ipam.vlans.all())
     return vlans
